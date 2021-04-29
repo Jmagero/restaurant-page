85a92edd974a83883c193b3b732d99d41c403405
Jocyline Magero <jmagero2017@gmail.com>
2021-04-29 06:36:46 +0300

    CreateHome function to add content to home tab

--- src/home.js
+++ src/home.js
@@ -1,17 +1,16 @@
-
 const createHome =() => {
     let about = document.createElement('div')
     let  title = document.createElement('h3')
-    content = document.getElementById("content")
-    about.setAttribute('id', 'home-tab')
-    about.classList.add('tabcontent')
+    about.setAttribute('id', 'tabcontent')
     title.classList.add('title')
     let para = document.createElement('p')
     para.classList.add('description')
     title.innerHTML = "Indulge yourself"
-    para.innerHTML = "Your restaurant’s mission statement is more than just a few words strung together. I"
+    para.innerHTML = " Eat, Drink Be Merry"
     about.append(title,para)
-    console.log(content.appendChild(about));
+    console.log(about);
+    return about;
 }
 
 export default createHome;
+
